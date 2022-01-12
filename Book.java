794329d2d450c43f4dd80b3bd41854d677ab8c64
rktkoichi <rokutakoichi@gmail.com>
2022-01-12 14:15:21 +0900

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,11 @@
+public class Book extends TngibleAsset {
+	private String isbn;
+	public Book (String neme, int price, String color, String isbn) {
+		super(neme,price,int);
+		this.isbn isbn;
+	}
+
+	public String getIsbn() { return this.isbn; }
+
+}
+		
